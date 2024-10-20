fb21eb366efe3eeb5be78f9bff15db70d7fe73d7
Nas <nassersanou23@gmail.com>
2024-10-21 00:17:00 +0200

    CheckWindowSize

--- src/utils/CheckWindowSize.js
+++ src/utils/CheckWindowSize.js
@@ -1,11 +1,31 @@
+'use client'
+
+import { useEffect, useState } from "react";
+
 // Function to check window size (mobile, iPad, desktop)
 export function CheckWindowSize() {
-    const width = window.innerWidth;
+
+  const [widowInstance , setWindowInstance] = useState()
+  const [widowWidth , setWindowWidth] = useState()
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setWindowInstance(true)
+      const width = window.innerWidth;
+       setWindowWidth(width)
+      console.log('PAGE_IN_COMPONENTS_DIR_This is client-side rendering');
+    } else {
+      setWindowInstance(undefined)
+
+      console.log('PAGE_IN_COMPONENTS_DIR_This is server-side rendering');
+    }
+  }, []);
+
+   
   
     if (width <= 768) {
       // Mobile screen size (width <= 768px)
       return 'mobile';
-    } else if (width > 768 && width <= 1024) {
+    } else if (widowWidth > 768 && widowWidth <= 1024) {
       // iPad screen size (768px < width <= 1024px)
       return 'ipad';
     } else {
